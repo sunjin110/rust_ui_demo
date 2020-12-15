6b66d226987235dfb9a0821465a47c04ed504189
yun_sunjin <yun_sunjin@cocone.co.jp>
2020-12-16 00:19:45 +0900

    a

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-
 use iced::Application;
 
-struct GUI;
+struct GUI {
+    start_stop_button_state: iced::button::State,
+    reset_button_state: iced::button::State,
+}
 
 impl iced::Application for GUI {
     type Executor = iced::executor::Null;
@@ -9,9 +11,13 @@ impl iced::Application for GUI {
     type Flags = ();
 
     fn new(_flags: ()) -> (GUI, iced::Command<Self::Message>) {
-
-        // 初期化の時点でやりたい処理がないためnone
-        (GUI, iced::Command::none())
+        (
+            GUI {
+                start_stop_button_state: iced::button::State::new(),
+                reset_button_state: iced::button::State::new(),
+            },
+            iced::Command::none(),
+        )
     }
 
     fn title(&self) -> String {
@@ -23,7 +29,41 @@ impl iced::Application for GUI {
     }
 
     fn view(&mut self) -> iced::Element<Self::Message> {
-        iced::Text::new("Hello, World").into()
+
+        // init widgets
+        let tick_text = iced::Text::new("00:00:00:00")
+            .font(iced::Font::Default)
+            .size(60);
+
+        let start_stop_button = iced::Button::new(
+            &mut self.start_stop_button_state,
+            iced::Text::new("Start")
+                .horizontal_alignment(iced::HorizontalAlignment::Center)
+                .font(iced::Font::Default),
+        )
+        .min_width(80);
+
+        let reset_button = iced::Button::new(
+            &mut self.reset_button_state,
+            iced::Text::new("Reset")
+                .horizontal_alignment(iced::HorizontalAlignment::Center)
+                .font(iced::Font::Default),
+        )
+        .min_width(80);
+
+        // prepare column
+        iced::Column::new().push(tick_text).push(
+            iced::Row::new()
+                .push(start_stop_button)
+                .push(reset_button)
+                .spacing(10),
+        )
+        .spacing(10)
+        .padding(10)
+        .width(iced::Length::Fill)
+        .height(iced::Length::Fill)
+        .align_items(iced::Align::Center)
+        .into()
     }
 }
 
